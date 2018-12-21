db8eef78c1ecb250c69711b336188b4979d30ce2
Daniel L <daniellatham11@gmail.com>
2018-12-21 15:11:50 -0600

    Add working genserver

---
+++ test/kv/bucket_test.exs
@@ -0,0 +1,17 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    bucket = start_supervised!(KV.Bucket)
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    KV.Bucket.put(bucket, "orange", 12)
+    assert KV.Bucket.get(bucket, "milk") == 3
+    assert KV.Bucket.get(bucket, "orange") == 12
+  end
+end
